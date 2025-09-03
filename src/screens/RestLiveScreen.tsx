c7a211f83568b56061277df44a0dcf67d77e45c0
Prithin <bprithin@applab.qa>
2025-09-03 12:39:14 +0530

    first commit

--- src/screens/RestLiveScreen.tsx
+++ src/screens/RestLiveScreen.tsx
@@ -1,45 +1,97 @@
-import { View, Text, Image, ScrollView, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, ActivityIndicator, FlatList, Pressable } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { fetchAPI } from '../components/contentful/fetchEntry';
 import { renderRichText } from '../components/contentful/RichTextRenderer';
 import { useRoute } from '@react-navigation/native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { networkApi } from '../https/api';
+import { url, urlEndPoints } from '../https/apiConfig';
+import { WIDTH, HEIGHT } from '../constants/dimensions';
 
 const RestLiveScreen = () => {
     const [entry, setEntry] = useState<any>(null);
     const route = useRoute()
     const { entry_id } = route?.params || {}
+    const [activityList, setActivityList] = useState()
+    const [page, setPage] = useState('1')
+    const [loader, setLoader] = useState(false)
+    const [contentLoader, setContentLoader] = useState(false)
 
     useEffect(() => {
+        setLoader(true)
+        setContentLoader(true)
+        getListAPI()
         fetchAPI(entry_id).then(async (response) => {
             setEntry(response)
+            setContentLoader(false)
             await AsyncStorage.setItem('cachedRestData', JSON.stringify(response));
-        });
+        }).catch((e) => {
+            setContentLoader(false)
+        });;
     }, []);
 
     const fields = entry?.fields || {};
 
+    const getListAPI = async () => {
+        try {
+            const apiUrl = `${url.laravelApi()}${urlEndPoints.activitiesList(page)}`
+            const response = await networkApi(apiUrl, 'GET')
+            if (response?.data?.response?.result) {
+                setActivityList(response?.data?.response?.result)
+                setLoader(false)
+            } else {
+                setLoader(false)
+                setActivityList([])
+            }
+        } catch (error) {
+            console.log("Error=>", error)
+        }
+    }
+
+
 
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <ScrollView contentContainerStyle={styles.container}>
-                <Text style={styles.header}>{fields.headerText}</Text>
-                {fields.description && renderRichText(fields.description)}
-
-                {/* Date Field */}
-                {fields.dateField && (
-                    <Text style={styles.dateText}>
-                        Date: {new Date(fields.dateField).toLocaleDateString()}
-                    </Text>
+        // <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        //     <ScrollView contentContainerStyle={styles.container}>
+        //         <Text style={styles.header}>{fields.headerText}</Text>
+        //         {fields.description && renderRichText(fields.description)}
+        <View style={{ flex: 1 }}>
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', width: WIDTH }}>
+                {contentLoader ? (
+                    <View style={{ alignItems: 'center', justifyContent: 'center', flex: 1, paddingVertical: HEIGHT * 0.08, width: '100%' }}>
+                        <ActivityIndicator size={'large'} />
+                    </View>
+                ) : (
+                    <View style={{ alignItems: 'center' }}>
+                        <Text style={styles.header}>Contentful</Text>
+                        {fields.description && renderRichText(fields.description)}
+                    </View>
                 )}
-
-                {/* JSON Field */}
-                {fields.jsonField && (
-                    <Text style={styles.jsonText}>
-                        JSON: {JSON.stringify(fields.jsonField)}
-                    </Text>
-                )}
-            </ScrollView>
+                <View style={{ borderWidth: 1, width: WIDTH, marginVertical: HEIGHT * 0.04 }} />
+                <Text style={[styles.header]}>
+                    TMS
+                </Text>
+                <FlatList
+                    data={activityList?.activities}
+                    contentContainerStyle={{ gap: HEIGHT * 0.02, width: WIDTH * 0.75 }}
+                    renderItem={({ item }) => (
+                        <Pressable style={{ borderWidth: 0.3, borderRadius: 8, paddingVertical: HEIGHT * 0.01, paddingHorizontal: WIDTH * 0.03 }}>
+                            <Text style={{ color: 'black', fontSize: 18 }}>{item?.title}</Text>
+                            <Text style={{ color: '#A1670D', fontSize: 12, paddingVertical: HEIGHT * 0.01 }}>{item?.date_time}</Text>
+                            <Text style={{ color: 'black', fontSize: 14 }} numberOfLines={2}>{item?.description}</Text>
+                        </Pressable>
+                    )}
+                    ListEmptyComponent={() => loader ? (
+                        <View style={{ alignItems: 'center', justifyContent: 'center', flex: 1, paddingVertical: HEIGHT * 0.08 }}>
+                            <ActivityIndicator size={'large'} />
+                        </View>
+                    ) : (!activityList?.activities && (
+                        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                            <Text>No data</Text>
+                        </View>
+                    ))}
+                />
+            </View>
         </View>
     )
 }
--- src/screens/SdkLiveScreen.tsx
+++ src/screens/SdkLiveScreen.tsx
@@ -1,32 +1,70 @@
-import { View, Text, ScrollView, StyleSheet } from 'react-native'
+/* eslint-disable react/no-unstable-nested-components */
+/* eslint-disable react-native/no-inline-styles */
+import { View, Text, StyleSheet, ActivityIndicator, FlatList, Pressable } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { fetchEntry } from '../components/contentful/fetchEntry';
 import { renderRichText } from '../components/contentful/RichTextRenderer';
 import { useRoute } from '@react-navigation/native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { networkApi } from '../https/api';
+import { url, urlEndPoints } from '../https/apiConfig';
+import { HEIGHT, WIDTH } from '../constants/dimensions';
 
 
 const SdkLiveScreen = () => {
     const [entry, setEntry] = useState<any>(null);
     const route = useRoute()
     const { entry_id } = route?.params || {}
+    const [activityList, setActivityList] = useState()
+    const [page, setPage] = useState('1')
+    const [loader, setLoader] = useState(false)
+    const [contentLoader, setContentLoader] = useState(false)
 
     useEffect(() => {
+        setLoader(true)
+        setContentLoader(true)
+        getListAPI()
         fetchEntry(entry_id).then(async (response) => {
             setEntry(response)
+            setContentLoader(false)
             await AsyncStorage.setItem('cachedSDKEntries', JSON.stringify(response));
+        }).catch((e) => {
+            setContentLoader(false)
         });
     }, []);
 
     const fields = entry?.fields || {};
 
+    const getListAPI = async () => {
+        try {
+            const apiUrl = `${url.laravelApi()}${urlEndPoints.activitiesList(page)}`
+            const response = await networkApi(apiUrl, 'GET')
+            if (response?.data?.response?.result) {
+                setActivityList(response?.data?.response?.result)
+                setLoader(false)
+            } else {
+                setLoader(false)
+                setActivityList([])
+            }
+        } catch (error) {
+            console.log("Error=>", error)
+        }
+    }
 
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', }}>
-            <ScrollView contentContainerStyle={styles.container}>
-                <Text style={styles.header}>{fields.headerText}</Text>
-                {fields.description && renderRichText(fields.description)}
-                {fields.dateField && (
+        <View style={{ flex: 1 }}>
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', width: WIDTH }}>
+                {contentLoader ? (
+                    <View style={{ alignItems: 'center', justifyContent: 'center', flex: 1, paddingVertical: HEIGHT * 0.08, width: '100%' }}>
+                        <ActivityIndicator size={'large'} />
+                    </View>
+                ) : (
+                    <View style={{alignItems: 'center'}}>
+                        <Text style={styles.header}>Contentful</Text>
+                        {fields.description && renderRichText(fields.description)}
+                    </View>
+                )}
+                {/* {fields.dateField && (
                     <Text style={styles.dateText}>
                         Date: {new Date(fields.dateField).toLocaleDateString()}
                     </Text>
@@ -35,8 +73,33 @@ const SdkLiveScreen = () => {
                     <Text style={styles.jsonText}>
                         JSON: {JSON.stringify(fields.jsonField?.['en-US'])}
                     </Text>
-                )}
-            </ScrollView>
+                )} */}
+
+                <View style={{ borderWidth: 1, width: WIDTH, marginVertical: HEIGHT * 0.04 }} />
+                <Text style={[styles.header]}>
+                    TMS
+                </Text>
+                <FlatList
+                    data={activityList?.activities}
+                    contentContainerStyle={{ gap: HEIGHT * 0.02, width: WIDTH * 0.75 }}
+                    renderItem={({ item }) => (
+                        <Pressable style={{ borderWidth: 0.3, borderRadius: 8, paddingVertical: HEIGHT * 0.01, paddingHorizontal: WIDTH * 0.03 }}>
+                            <Text style={{ color: 'black', fontSize: 18 }}>{item?.title}</Text>
+                            <Text style={{ color: '#A1670D', fontSize: 12, paddingVertical: HEIGHT * 0.01 }}>{item?.date_time}</Text>
+                            <Text style={{ color: 'black', fontSize: 14 }} numberOfLines={2}>{item?.description}</Text>
+                        </Pressable>
+                    )}
+                    ListEmptyComponent={() => loader ? (
+                        <View style={{ alignItems: 'center', justifyContent: 'center', flex: 1, paddingVertical: HEIGHT * 0.08 }}>
+                            <ActivityIndicator size={'large'} />
+                        </View>
+                    ) : (!activityList?.activities && (
+                        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                            <Text>No data</Text>
+                        </View>
+                    ))}
+                />
+            </View>
         </View>
     )
 }
